597d88834765d2a2cd67bd9c0bcddf2f3d216f3a
crisxian <crisxian@users.noreply.github.com>
2015-03-23 08:06:19 -0700

    Exercise framework
    Core files for starting a fresh twitter clone project

---
+++ message-maker.js
@@ -0,0 +1,10 @@
+/*
+* NOTE: This file generates fake message data, re-implement this yourself if you want to.
+* You can use this file and just make changes in the index.html
+*/
+
+// set up data structures 1) homepage tweets 2) users
+
+// function to add tweet data
+
+// function to submit tweets
